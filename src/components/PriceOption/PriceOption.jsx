0193b69fb158d4ee5402fca6eafa2199a09c4a0c
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2025-02-20 20:44:08 +0600

    visual some props property

--- src/components/PriceOption/PriceOption.jsx
+++ src/components/PriceOption/PriceOption.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const PriceOption = ({option}) => {
-  const {name, features, price} = option;
+const PriceOption = ({ option }) => {
+  const { name, features, price } = option;
   return (
-    <div>
+    <div className="flex mx-auto">
+      <span className="text-3xl text-red-400">{name}</span>
       <h2>
-        <span className='text-7xl'>{price}</span>
-        <span className='text-3xl'>/mon</span>
-        </h2>
+        <span className="text-7xl">{price}</span>
+
+        <span className="text-3xl">/mon</span>
+      </h2>
+      <div>
+        <span className="text-2xl text-gray-500">{features}</span>
+      </div>
     </div>
   );
 };
