063b5881c0f692ab6dfeab92ceb2eacfb1e8a4ac
Md. Ashifur Rahaman Sajib <sksajib111@gmail.com>
2025-02-23 10:55:30 +0600

    create a new feature component

---
+++ src/components/Feature/Feature.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Feature = ({feature}) => {
+  return (
+    <div>
+      <p className='text-bold text-center'>{feature}</p>
+    </div>
+  );
+};
+
+export default Feature;
--- src/components/PriceOption/PriceOption.jsx
+++ src/components/PriceOption/PriceOption.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Feature from "../Feature/Feature";
 
 const PriceOption = ({ option }) => {
   const { name, features, price } = option;
@@ -8,7 +9,10 @@ const PriceOption = ({ option }) => {
         <span className="text-7xl font-extrabold">{price}</span>
         <span className="text-3xl">/mon</span>
       </h2>
-      <h4 className="text-3xl text-center">{name}</h4>
+      <h4 className="text-3xl text-center mb-10">{name}</h4>
+      {
+        features.map((feature, index) => <Feature key={index} feature={feature}></Feature>)
+      }
     </div>
   );
 };
